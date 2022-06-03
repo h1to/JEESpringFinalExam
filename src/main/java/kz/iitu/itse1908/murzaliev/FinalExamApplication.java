b503a4915b59c0f04cfad02db5d7d20d2b176cbf
eldiabloo <1208@313.kz>
2022-06-03 11:04:22 +0600

    First commit

---
+++ src/main/java/kz/iitu/itse1908/murzaliev/FinalExamApplication.java
@@ -0,0 +1,13 @@
+package kz.iitu.itse1908.murzaliev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalExamApplication.class, args);
+    }
+
+}
